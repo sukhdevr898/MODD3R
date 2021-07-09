cb68507620da07d8e4f616d2d6e468def001642c
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2021-07-09 05:13:37 +0000

    Add files via upload

---
+++ mod.sh
@@ -0,0 +1,100 @@
+
+
+
+
+
+echo -e "THIS TOOL ONLY FOR MODDING TBOMB  "
+ echo -e "\e[1;35m ███╗░░░███╗░█████╗░██████╗░██████╗░██████╗░██████╗"░
+ echo -e "\e[1;35m ████╗░████║██╔══██╗██╔══██╗██╔══██╗╚════██╗██╔══██╗"
+ echo -e "\e[1;35m ██╔████╔██║██║░░██║██║░░██║██║░░██║░█████╔╝██████╔╝"
+ echo -e "\e[1;35m ██║╚██╔╝██║██║░░██║██║░░██║██║░░██║░╚═══██╗██╔══██╗"
+ echo -e "\e[1;35m ██║░╚═╝░██║╚█████╔╝██████╔╝██████╔╝██████╔╝██║░░██║"
+ echo -e "\e[1;35m ╚═╝░░░░░╚═╝░╚════╝░╚═════╝░╚═════╝░╚═════╝░╚═╝░░╚═╝"
+ echo -e "\e[1;32m \n\t CREATED BY SUKHDEVR898"
+ echo -e "\e[1;36m \n\t CHOOSE A OPTION "
+ echo -e "\e[1;33m 1 => MOD TBOMB" 
+echo -e "\e[1;33m 2 => UPDATE "
+ echo -e "\e[1;33m 3 => EXIT "
+read -p "ENTER ANY NO. : " OP
+case "$OP" in  
+1)
+echo -e "\e[1;44m TYPE Y OR N "
+read -p " DO YOU WANT TO MOD TBOMB : " m
+;;
+2)
+echo -e "\e[1;45m UPDATE NOT AVAILABLE"
+;;
+3)
+echo -e "EXITING..."
+;;
+esac
+case "$m" in 
+y)
+clear
+echo -e "\e[1;33m MODDING. "
+sleep 0.5
+clear
+echo -e "\e[1;33m MODDING.. "
+sleep 0.5
+clear
+echo -e "\e[1;33m MODDING... "
+sleep 0.5
+clear
+echo -e "\e[1;33m MODDING. "
+sleep 0.5
+clear
+echo -e "\e[1;33m MODDING.. "
+sleep 0.5
+clear
+echo -e "\e[1;33m MODDING... "
+sleep 0.5
+clear
+cp bomber.py $HOME/TBomb
+figlet -f big DONE... 
+;;
+Y)
+clear
+echo -e "\e[1;33m MODDING. "
+sleep 0.5
+clear
+echo -e "\e[1;33m MODDING.. "
+sleep 0.5
+clear
+echo -e "\e[1;33m MODDING... "
+sleep 0.5
+clear
+echo -e "\e[1;33m MODDING. "
+sleep 0.5
+clear
+echo -e "\e[1;33m MODDING.. "
+sleep 0.5
+clear
+echo -e "\e[1;33m MODDING... "
+sleep 0.5
+clear
+cp bomber.py $HOME/TBomb
+figlet -f big DONE...
+;;
+n) 
+clear 
+echo -e '\e[1;33m EXITING.'
+ sleep 0.5
+ clear
+ echo -e '\e[1;33m EXITING..'
+ sleep 0.5 
+clear
+ echo -e '\e[1;33m EXITING...' 
+sleep 0.5
+ clear
+ echo -e '\e[1;33m EXITING.'
+ sleep 0.5
+ clear
+ echo -e '\e[1;33m EXITING..'
+ sleep 0.5
+ clear
+ echo -e '\e[1;33m EXITING...'
+ sleep 0.5
+ clear
+bash mod.sh
+;;
+esac
