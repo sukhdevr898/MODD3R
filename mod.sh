a2ab5e82bb5e855ca920cd03bd4b5a636bd65f6b
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2021-10-20 14:32:46 +0000

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -1,8 +1,8 @@
 
-
+upd_url="https://github.com/sukhdevr898/MODD3R"
 clear
 
-
+echo-e "\e[98m ___________________________________________________________________"
 echo -e "THIS TOOL ONLY FOR MODDING TBOMB  "
  echo -e "\e[1;35m ███╗░░░███╗░█████╗░██████╗░██████╗░██████╗░██████╗"░
  echo -e "\e[1;35m ████╗░████║██╔══██╗██╔══██╗██╔══██╗╚════██╗██╔══██╗"
@@ -10,21 +10,61 @@ echo -e "THIS TOOL ONLY FOR MODDING TBOMB  "
  echo -e "\e[1;35m ██║╚██╔╝██║██║░░██║██║░░██║██║░░██║░╚═══██╗██╔══██╗"
  echo -e "\e[1;35m ██║░╚═╝░██║╚█████╔╝██████╔╝██████╔╝██████╔╝██║░░██║"
  echo -e "\e[1;35m ╚═╝░░░░░╚═╝░╚════╝░╚═════╝░╚═════╝░╚═════╝░╚═╝░░╚═╝"
- echo -e "\e[1;32m \n\t CREATED BY SUKHDEVR898 \n\t\t version 1.1 antiban"
+ echo -e "\e[1;32m \n\t CREATED BY SUKHDEVR898 \n\t\t version 2.0 antiban"
 
+echo-e "\e[98m ___________________________________________________________________"
+
+echo-e "\e[98m ====================================================================="
  echo -e "\e[1;36m \n\t CHOOSE A OPTION "
  echo -e "\e[1;33m 1 => MOD TBOMB" 
 echo -e "\e[1;33m 2 => UPDATE "
  echo -e "\e[1;33m 3 => EXIT "
+echo-e "\e[98m ====================================================================="
+ 
 read -p "ENTER ANY NO. : " OP
 case "$OP" in  
 1)
+echo -e "\e[98m CHECKING TBOMB..."
+if [ -d TBomb ] ;
+then 
+
 echo -e "\e[0;44m TYPE Y OR N "
 read -p " DO YOU WANT TO MOD TBOMB : " m
 ;;
 2)
-echo -e "\e[1;45m PLEASE WAIT..."
-git clone https://github.com/sukhdevr898/MODD3R
+echo -e "\e[98m\t\t\t PLEASE WAIT.....!"
+echo -e "\e[98m\t\t\t UPDATE CHECKING.....!"
+sleep 5.0
+wget -q --spider https://raw.githubusercontent.com/sukhdevr898/MODD3R/main/.version2.0
+if [ $? -eq 0 ];
+then 
+echo -e "\e[96m\t\t\t |------------------------------------------------------|"
+echo -e "\e[96m\t\t\t |    TOOL ARLEADY UPDATED VERSION    [✓]               |"
+echo -e "\e[96m\t\t\t |------------------------------------------------------|"
+
+else 
+echo -e "\e[96m\t\t\t |------------------------------------------------------|"   
+echo -e "\e[96m\t\t\t |    UPDATE AVAILABLE  [✓]                             |"
+echo -e "\e[96m\t\t\t |------------------------------------------------------|"
+fi
+echo -e "DO YOU WANT TO UPDATE THIS TOOL "
+read -p "Y" UP
+case $UP in
+
+y)
+cd $HOME
+rm -rf MODD3R
+git clone $upd_url
+cd MODD3R
+bash mod.sh
+;;
+Y)
+cd $HOME
+rm -rf MODD3R
+git clone $upd_url
+cd MODD3R
+bash mod.sh
+;;
 ;;
 3)
 echo -e "EXITING..."
@@ -51,7 +91,6 @@ clear
 echo -e "\e[1;33m MODDING... "
 sleep 0.5
 clear
-wget https://download1487.mediafire.com/3gej50i7yefg/id8844aizubn064/bomber.py
 cp bomber.py $HOME/TBomb
 figlet -f big DONE... 
 ;;
@@ -75,7 +114,7 @@ clear
 echo -e "\e[1;33m MODDING... "
 sleep 0.5
 clear
-wget https://download1487.mediafire.com/3gej50i7yefg/id8844aizubn064/bomber.py
+
 cp bomber.py $HOME/TBomb
 figlet -f big DONE...
 ;;
