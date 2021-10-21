3116419b87fe13f4933224f9064fc61326009582
sukhdevr898 <59791782+sukhdevr898@users.noreply.github.com>
2021-10-21 12:40:32 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,16 @@
+####DONT COPY
+wget -q --spider https://raw.githubusercontent.com/sukhdevr898/sukhdevr898/main/.MODD3R
+if [ $? -eq 0 ];
+then 
+echo -e "\e[96m\t\t\t |------------------------------------------------------|"
+echo -e "\e[96m\t\t\t |    CONNECTED  [✓]                                    |"
+echo -e "\e[96m\t\t\t |------------------------------------------------------|"
+echo -e "\e[96m\n\t\t\t |========= YOU ARE NOW CONNECTED WITH OUR SITE ✓ ===|"
+else 
+echo -e "\e[96m\t\t\t |------------------------------------------------------|"   
+echo -e "\e[96m\t\t\t |    NOT CONNECTED [×]                                 |"
+echo -e "\e[96m\t\t\t |------------------------------------------------------|"
+echo -e "\e[96m\t\t\t |   TOOL DESTROYED BY SUKHDEVR898 YOU CAN'T USE        |"
+echo -e "\e[96m\t\t\t |------------------------------------------------------|"
+ 
+fi
